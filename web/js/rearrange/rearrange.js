5b173e6bf831ce61ca97dbb5d117bd3fa798da78
Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>
2015-03-27 15:59:29 +0000

    Updates to Rearrangeable Clients/Work
    Signed-off-by: Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>

---
+++ web/js/rearrange/rearrange.js
@@ -0,0 +1,19 @@
+	var body = document.body,
+	dropArea = document.getElementById( 'drop-area' );
+
+	$('.rearrangeable-trigger-btn').on('click', function() {
+		classie.add( body, 'drag-active' );
+		classie.add( dropArea, 'show' );
+
+		var url = $(this).attr("data-url");
+		$.ajax(url).done(function(data) {
+			$('#drop-area .main-body').html(data);
+		});
+	})
+
+	$('#drop-area .close-character').on('click', function() {
+		classie.remove( body, 'drag-active' );
+		classie.remove( dropArea, 'show' );
+		$('#drop-area .main-body').empty();
+	})
+
