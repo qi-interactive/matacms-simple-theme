f504be8277d57816286c39d2357d7d5b6c373802
Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>
2015-04-09 12:47:06 +0100

    Updates
    Signed-off-by: Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>

--- web/js/rearrange/rearrange.js
+++ web/js/rearrange/rearrange.js
@@ -1,22 +1,21 @@
-	var body = document.body,
-	dropArea = document.getElementById( 'drop-area' );
+var body = document.body,
+dropArea = document.getElementById( 'drop-area' );
 
-	$('.rearrangeable-trigger-btn').on('click', function() {
-		classie.add( body, 'drag-active' );
-		classie.add( dropArea, 'show' );
+$('.rearrangeable-trigger-btn').on('click', function() {
+	classie.add( body, 'drag-active' );
+	classie.add( dropArea, 'show' );
 
 		// var url = $(this).attr("data-url");
 		// $.ajax(url).done(function(data) {
 		// 	$('#drop-area .main-body').html(data);
 		// });
-	$('#drop-area .main-body iframe').attr("src", $(this).attr("data-url"))
+$('#drop-area .main-body iframe').attr("src", $(this).attr("data-url"))
 
-	})
+})
 
-	$('#drop-area .close-character').on('click', function() {
-		classie.remove( body, 'drag-active' );
-		classie.remove( dropArea, 'show' );
-		$('#drop-area .main-body iframe').attr("src", "")
+$('#drop-area .close-character').on('click', function() {
+	classie.remove( body, 'drag-active' );
+	classie.remove( dropArea, 'show' );
+	$('#drop-area .main-body iframe').attr("src", "")
 
-	})
-
+})
