55c7bd63e73f7d2b4cf6f81ec023bf23f7a0c887
Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>
2015-04-24 12:44:00 +0100

    Updates
    Signed-off-by: Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>

--- web/js/rearrange/rearrange.js
+++ web/js/rearrange/rearrange.js
@@ -2,8 +2,10 @@ var body = document.body,
 dropArea = document.getElementById( 'drop-area' );
 
 $('.rearrangeable-trigger-btn').on('click', function() {
+	alert(1);
 	classie.add( body, 'drag-active' );
-	
+	$('.cd-header').addClass('disable');
+
 	var url = $(this).attr("data-url");
 	$.ajax(url).done(function(data) {
 		$('#drop-area .main-body').html(data);
@@ -16,5 +18,6 @@ $('#drop-area .close-character').on('click', function() {
 	classie.remove( body, 'drag-active' );
 	classie.remove( dropArea, 'show' );
 	$('#drop-area .main-body').empty();
+	$('.cd-header').removeClass('disable');
 
 })
