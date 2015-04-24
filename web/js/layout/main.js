fb127ae1a8ca9eb0bd64c9e0957ce2213c67930d
Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>
2015-04-24 15:20:53 +0100

    Removed unwanted codes
    Signed-off-by: Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>

--- web/js/layout/main.js
+++ web/js/layout/main.js
@@ -159,14 +159,6 @@ jQuery(document).ready(function($) {
 	};
 });
 
-mata.addHeightToContainer = function() { 
-	if($('.cd-3d-nav-container').hasClass('nav-is-visible')) {
-		$('#container').height($(this).height() - ($('.cd-header').height() + $('.cd-3d-nav-container').height()));
-	} else {
-		$('#container').height($(this).height() - $('.cd-header').height());
-	}
-}
-
 
 
 $(window).ready(function() {
@@ -179,6 +171,7 @@ $(window).ready(function() {
 		that = $(this);
 		addHeightToContainer(that)
 	}).resize();
+
 
 });
 
