416a6be48e8f5bf9934bed6dff4386b7f20bc491
Michal Fiedorowicz <michal@qi-interactive.com>
2015-04-28 17:26:40 +0100

    Updates

--- web/js/layout/theme.js
+++ web/js/layout/theme.js
@@ -83,7 +83,7 @@ mata.simpleTheme.addSelectedAscendingDescendingClassesToSortBy = function() {
 
 mata.simpleTheme.addAClassToSelectizeAndFileUploaderAndUriParent = function() {
 	$('select, .file-uploader, #generate-uri').each(function(el) {
-		$(this).parents('.form-group').addClass('partial-max-width-item');
+		$(this).parents('.form-group:not(.full-width)').addClass('partial-max-width-item');
 	})
 }
 
