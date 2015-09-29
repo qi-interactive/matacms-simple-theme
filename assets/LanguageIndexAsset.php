a2d4b849eb8681f935f07bdb06a8578c0b7a702c
Michal Fiedorowicz <michal@qi-interactive.com>
2015-09-29 10:15:52 +0100

    Added assets for matacms-language module

---
+++ assets/LanguageIndexAsset.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @link http://www.matacms.com/
+ * @copyright Copyright (c) 2015 Qi Interactive Limited
+ * @license http://www.matacms.com/license/
+ */
+
+namespace matacms\theme\simple\assets;
+
+use yii\web\AssetBundle;
+
+class LanguageIndexAsset extends AssetBundle
+{
+
+    public $sourcePath = '@vendor/matacms/matacms-simple-theme/web';
+
+    public $css = [
+	    'css/language/language.css',
+    ];
+
+}
